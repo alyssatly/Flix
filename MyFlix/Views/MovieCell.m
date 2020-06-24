e3d9a4e369069f4642ea2cc68cb3121c87213f95
alyssatly <alyssatan2010@gmail.com>
2020-06-24 14:10:15 -0700

    User can scroll through a list of movies currently playing in theaters from The Movie DB API

---
+++ MyFlix/Views/MovieCell.m
@@ -0,0 +1,24 @@
+//
+//  MovieCell.m
+//  MyFlix
+//
+//  Created by Alyssa Tan on 6/24/20.
+//  Copyright © 2020 Alyssa Tan. All rights reserved.
+//
+
+#import "MovieCell.h"
+
+@implementation MovieCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Pods/Target Support Files/Pods-MyFlix/Pods-MyFlix-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyFlix : NSObject
+@end
+@implementation PodsDummy_Pods_MyFlix
+@end
