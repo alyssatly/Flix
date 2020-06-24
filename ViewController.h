3f9a10a79d0a249b65b8a32d5a25039f09574359
alyssatly <alyssatan2010@gmail.com>
2020-06-24 10:17:45 -0700

    App Icon and Launch Screen

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFlix
+//
+//  Created by Alyssa Tan on 6/24/20.
+//  Copyright © 2020 Alyssa Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
