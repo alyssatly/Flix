1b9b1555f3d372d3ba979d3603481b98077cd6d1
alyssatly <alyssatan2010@gmail.com>
2020-06-25 13:29:36 -0700

    Added alert when offline

--- MyFlix/View Controllers/MoviesViewController.m
+++ MyFlix/View Controllers/MoviesViewController.m
@@ -49,6 +49,25 @@
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
            if (error != nil) {
                NSLog(@"%@", [error localizedDescription]);
+               UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies"
+                      message:@"The Internet Connection Appears to be offline"
+               preferredStyle:(UIAlertControllerStyleAlert)];
+               
+               UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again"
+                                                                   style:UIAlertActionStyleCancel
+                                                                 handler:^(UIAlertAction * _Nonnull action) {
+                                                                        [self.activityIndicator startAnimating];
+                                                                        [self fetchMovies];
+                                            
+                                                                 }];
+               // add the try again action to the alertController
+               [alert addAction:tryAgainAction];
+               
+               [self presentViewController:alert animated:YES completion:^{
+                   // optional code for what happens after the alert controller has finished presenting
+               }];
+
+               
            }
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
